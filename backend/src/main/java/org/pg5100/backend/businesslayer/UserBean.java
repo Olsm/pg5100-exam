0a822c1229f1b5b6e4a5e717dd2a49930373cd12
Olsm <OlavOlsm@gmail.com>
2016-10-17 15:39:45 +0200

    add UserBean and tests

---
+++ backend/src/main/java/org/pg5100/backend/businesslayer/UserBean.java
@@ -0,0 +1,95 @@
+package org.pg5100.backend.businesslayer;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.pg5100.backend.datalayer.Address;
+import org.pg5100.backend.datalayer.User;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.validation.constraints.NotNull;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+@Stateless
+public class UserBean {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public UserBean(){}
+
+    /**
+     * Return created User object if the user was created successfully
+     * Return null if for any reason it was not possible to create the user
+     */
+    public User registerNewUser(@NotNull String username, @NotNull String password, @NotNull String email, String name, Address address){
+        if(isRegistered(username) || password.isEmpty()){
+            return null;
+        }
+
+        User user = new User(username, address, name, email);
+
+        //create a "strong" random string of at least 128 bits, needed for the "salt"
+        String salt = getSalt();
+        String hash = computeHash(password, salt);
+
+        user.setSalt(salt);
+        user.setHash(hash);
+
+        em.persist(user);
+        return user;
+    }
+
+    public boolean isRegistered(@NotNull String username){
+        return getUser(username) != null;
+    }
+
+    public int getNumberOfUsers(){
+        Query query = em.createQuery("select count(u) from User u");
+        return ((Number)query.getSingleResult()).intValue();
+    }
+
+    public User getUser(@NotNull String username) {
+        return em.find(User.class, username.toLowerCase());
+    }
+
+    // Return true if a user with the given password exists
+    public boolean login(String userName, String password) {
+        if (userName == null || userName.isEmpty() || password == null || password.isEmpty()) {
+            return false;
+        }
+
+        User userDetails = getUser(userName);
+        if (userDetails == null) {
+            return false;
+        }
+
+        String hash = computeHash(password, userDetails.getSalt());
+
+        return hash.equals(userDetails.getHash());
+    }
+
+
+    //------------------------------------------------------------------------
+
+
+    @NotNull
+    protected String computeHash(String password, String salt){
+        String combined = password + salt;
+        return DigestUtils.sha256Hex(combined);
+    }
+
+    @NotNull
+    protected String getSalt(){
+        SecureRandom random = new SecureRandom();
+        int bitsPerChar = 5;
+        int twoPowerOfBits = 32; // 2^5
+        int n = 26;
+        assert n * bitsPerChar >= 128;
+
+        return new BigInteger(n * bitsPerChar, random).toString(twoPowerOfBits);
+    }
+
+}
---
+++ backend/src/test/java/org/pg5100/backend/UserBeanTest.java
@@ -0,0 +1,181 @@
+package org.pg5100.backend;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.spi.ArquillianProxyException;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.pg5100.backend.businesslayer.UserBean;
+import org.pg5100.backend.datalayer.Address;
+import org.pg5100.backend.datalayer.User;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(Arquillian.class)
+public class UserBeanTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClasses(UserBean.class, User.class, Address.class)
+                .addPackages(true, "org.apache.commons.codec")
+                .addAsResource("META-INF/persistence.xml");
+    }
+
+    @EJB
+    private UserBean userEJB;
+
+    private User createUser(String userName, String password){
+        return createUser(userName, password, "shiba@inu.wow");
+    }
+
+    private User createUser(String userName, String password, String email){
+        return userEJB.registerNewUser(userName, password, email, "Satoshi Nakamoto", new Address("Suchcity", "Verycountry"));
+    }
+
+    @Test
+    public void testRegisterNewUser() {
+        String username = "SuchUserName";
+        User user = createUser(username, "password");
+        assertEquals(username.toLowerCase(), user.getUsername());
+        assertEquals("Suchcity", user.getAddress().getCity());
+        assertEquals("Verycountry", user.getAddress().getCountry());
+        assertEquals("Satoshi Nakamoto", user.getName());
+        assertEquals("shiba@inu.wow", user.getEmail());
+    }
+
+    @Test
+    public void testGetNumberOfUsers() {
+        int users = userEJB.getNumberOfUsers();
+        createUser("suchUser", "password");
+        assertEquals(users+1, userEJB.getNumberOfUsers());
+    }
+
+    @Test(expected = ArquillianProxyException.class)
+    public void testUserMustHaveUsername() {
+        createUser(null, "password");
+    }
+
+    @Test(expected = EJBException.class)
+    public void testUserCannotBeEmpty() {
+        createUser("   ", "password");
+    }
+
+    @Test(expected = EJBException.class)
+    public void testUserMustBeMin3Chars() {
+        createUser("12", "password");
+    }
+
+    public void testUserWithMinChars() {
+        createUser("123", "password");
+    }
+
+    @Test(expected = EJBException.class)
+    public void testUserMustBeMax15Chars() {
+        createUser("1234567890123456", "password");
+    }
+
+    public void testUserWithMaxChars() {
+        createUser("123456789012345", "password");
+    }
+
+    @Test(expected = EJBException.class)
+    public void testUserWithInvalidSpecialChar() {
+        createUser("123*abc", "password");
+    }
+
+    public void testUserWithValidSpecialChars() {
+        createUser("1_2-3", "password");
+    }
+
+    @Test(expected = EJBException.class)
+    public void testEmailWithoutDomain() {
+        createUser("invalidEmail1", "password", "invalid@email");
+    }
+
+    @Test(expected = EJBException.class)
+    public void testEmailWithoutAt() {
+        createUser("invalidEmail2", "password", "invalidemail.wow");
+    }
+
+    @Test(expected = EJBException.class)
+    public void testEmailWithoutAtAndDomain() {
+        createUser("invalidEmail3", "password", "invalidemail");
+    }
+
+    @Test
+    public void testNoTwoUsersWithSameId(){
+
+        String userName = "sameusername";
+
+        boolean created = createUser(userName,"a") != null;
+        assertTrue(created);
+
+        created = createUser(userName,"b") != null;
+        assertFalse(created);
+    }
+
+    @Test
+    public void testSamePasswordLeadToDifferentHashAndSalt(){
+
+        String password = "password";
+        String first = "first";
+        String second = "second";
+
+        createUser(first,password);
+        createUser(second,password); //same password
+
+        User f = userEJB.getUser(first);
+        User s = userEJB.getUser(second);
+
+        //those are EXTREMELY unlikely to be equal, although not impossible...
+        //however, likely more chances to get hit in the head by a meteorite...
+        assertNotEquals(f.getHash(), s.getHash());
+        assertNotEquals(f.getSalt(), s.getSalt());
+    }
+
+    @Test
+    public void testVerifyPassword(){
+
+        String user = "verifypass";
+        String correct = "correct";
+        String wrong = "wrong";
+
+        createUser(user, correct);
+
+        boolean  canLogin = userEJB.login(user, correct);
+        assertTrue(canLogin);
+
+        canLogin = userEJB.login(user, wrong);
+        assertFalse(canLogin);
+    }
+
+    @Test
+    public void testBeSurePasswordIsNotStoredInPlain(){
+        String password = "password";
+        User user = createUser("plain", password);
+
+        assertNotEquals(password, user.getUsername());
+        assertNotEquals(password, user.getHash());
+        assertNotEquals(password, user.getSalt());
+    }
+
+    /*
+    @Test(expected = EJBException.class)
+    public void testCreateAUserWithWrongCountry(){
+
+        String user = "wrongcountry";
+        String password = "password";
+
+        userEJB.createUser(user,password,"a","b","c","FOO");
+    }
+    */
+
+}
